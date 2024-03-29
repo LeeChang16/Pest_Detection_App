c5db70f914b996500702882d2613f554e1e915cf
LeeChang16 <chandleerey54@gmail.com>
2024-03-29 11:51:30 +0800

    Added Splash Screen and  Hashing algorithm

---
+++ app/src/main/java/com/example/pestdetectionapp/Pest_info.java
@@ -0,0 +1,14 @@
+package com.example.pestdetectionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pest_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pest_info);
+    }
+}
---
+++ app/src/main/java/com/example/pestdetectionapp/realtime_resultData.java
@@ -0,0 +1,20 @@
+package com.example.pestdetectionapp;
+
+import android.graphics.Bitmap;
+
+import java.sql.Blob;
+
+public class pest_data_recycler {
+
+     String name;
+     String scientific_name;
+//     Bitmap Image;
+
+    pest_data_recycler(String name, String scientific_name){
+
+        this.name = name;
+        this.scientific_name = scientific_name;
+//        this.Image = Image;
+
+    }
+}
